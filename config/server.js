b4633c93bcd065a2fc59cc11a4185febf9488f05
ben <kx8bcz5bw@relay.firefox.com>
2021-02-18 16:14:00 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '15e62ab23100b0f21ea9dc04b60dca96'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'df7c66e0-9af8-440e-8a20-c57161c7eaf3'
+};
