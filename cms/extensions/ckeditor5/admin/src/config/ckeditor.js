b1da5db71899d35003bc32db91e49e74f7305bc8
ben <kx8bcz5bw@relay.firefox.com>
2021-02-27 13:05:24 -0500

    create CKEitor config

---
+++ cms/extensions/ckeditor5/admin/src/config/ckeditor.js
@@ -0,0 +1,100 @@
+module.exports = {
+    ////// CONFIGURATION TEMPLATE: uncomment desired lines to override default config.
+    ////// While all lines are commented, config will not change anything.
+    toolbar: {
+        items: [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "underline",
+            "link",
+            "bulletedList",
+            "numberedList",
+            "imageInsert",
+            "strapiMediaLib",
+            "|",
+            "blockQuote",
+            "horizontalLine",
+            "mediaEmbed",
+            "insertTable",
+            "|",
+            "fullScreen",
+            "undo",
+            "redo",
+        ],
+        shouldNotGroupWhenFull: false,
+    },
+    // image: {
+    //     styles: ["alignLeft", "alignCenter", "alignRight"],
+    //     resizeOptions: [
+    //         {
+    //             name: "imageResize:original",
+    //             value: null,
+    //             icon: "original",
+    //         },
+    //         {
+    //             name: "imageResize:50",
+    //             value: "50",
+    //             icon: "medium",
+    //         },
+    //         {
+    //             name: "imageResize:75",
+    //             value: "75",
+    //             icon: "large",
+    //         },
+    //     ],
+    //     toolbar: [
+    //         "imageStyle:alignLeft",
+    //         "imageStyle:alignCenter",
+    //         "imageStyle:alignRight",
+    //         "|",
+    //         "imageTextAlternative",
+    //         "|",
+    //         "imageResize:50",
+    //         "imageResize:75",
+    //         "imageResize:original",
+    //         "|",
+    //         "linkImage",
+    //     ],
+    // },
+    table: {
+        contentToolbar: ["tableColumn", "tableRow"],
+    },
+    // heading: {
+    //     options: [
+    //         {
+    //             model: "paragraph",
+    //             title: "Paragraph",
+    //             class: "ck-heading_paragraph",
+    //         },
+    //         {
+    //             model: "heading1",
+    //             view: "h1",
+    //             title: "Heading 1",
+    //             class: "ck-heading_heading1",
+    //         },
+    //         {
+    //             model: "heading2",
+    //             view: "h2",
+    //             title: "Heading 2",
+    //             class: "ck-heading_heading2",
+    //         },
+    //         {
+    //             model: "heading3",
+    //             view: "h3",
+    //             title: "Heading 3",
+    //             class: "ck-heading_heading3",
+    //         },
+    //         {
+    //             model: "heading4",
+    //             view: "h4",
+    //             title: "Heading 4",
+    //             class: "ck-heading_heading4",
+    //         },
+    //     ],
+    // },
+    // htmlEmbed: {
+    //     showPreviews: true,
+    // },
+};
