e9d8116c4719f426a71f24bbfb2f5ee6ad1580a7
ben <kx8bcz5bw@relay.firefox.com>
2021-03-06 22:22:18 -0500

    feat(frontend): list projects on the projects page

--- frontend/src/pages/projects.js
+++ frontend/src/pages/projects.js
@@ -1,2 +1,36 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Layout from "../components/layout";
+
+const ProjectsPage = ({ data }) => {
+    console.log(data);
+    const projects = data.allStrapiProject.edges;
+    console.log(projects);
+    return (
+        <Layout>
+            {projects.map(({ node: { project_name, id } }) => {
+                return (
+                    <div key={id}>
+                        <h2>{project_name}</h2>
+                    </div>
+                );
+            })}
+        </Layout>
+    );
+};
+
+export default ProjectsPage;
+
+export const query = graphql`
+    {
+        allStrapiProject {
+            edges {
+                node {
+                    date
+                    project_name
+                    id
+                }
+            }
+        }
+    }
+`;
