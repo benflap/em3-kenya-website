213e0c4b3cb22272ad28852c6f3978e5f810cd59
ben <kx8bcz5bw@relay.firefox.com>
2021-03-06 22:14:05 -0500

    feat(frontend): show blog posts from cms on the blog page

--- frontend/src/pages/blog.js
+++ frontend/src/pages/blog.js
@@ -1,6 +1,37 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Layout from "../components/layout";
 
-const BlogPage = ({ data }) => {};
+const BlogPage = ({ data }) => {
+    console.log(data);
+    const blogPosts = data.allStrapiBlog.edges;
+    console.log(blogPosts);
+    return (
+        <Layout>
+            {blogPosts.map(({ node: { published_at, Title, id } }) => {
+                return (
+                    <article key={id}>
+                        <h2>{Title}</h2>
+                        <p>{published_at}</p>
+                    </article>
+                );
+            })}
+        </Layout>
+    );
+};
 
 export default BlogPage;
+
+export const query = graphql`
+    {
+        allStrapiBlog {
+            edges {
+                node {
+                    published_at
+                    Title
+                    id
+                }
+            }
+        }
+    }
+`;
