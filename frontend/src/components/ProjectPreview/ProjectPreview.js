507765574c980a16eb9e2e44c108e61f8e68f233
ben <benflap@protonmail.com>
2021-04-29 11:12:00 -0400

    create basic styles for pojects page

---
+++ frontend/src/components/ProjectPreview/ProjectPreview.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
+import * as styles from "./styles.module.css";
+
+const PostPreview = (props) => {
+    const { title, slug, image, imageAlt } = props;
+    const gatsbyImage = getImage(image);
+
+    return (
+        <article className={styles.postPreview}>
+            <Link to={`/blog/${slug}`} className={styles.link}>
+                <GatsbyImage
+                    image={gatsbyImage}
+                    alt={imageAlt}
+                    className={styles.image}
+                />
+                <div className={styles.lowerCard}>
+                    <h2 className={styles.title}>{title}</h2>
+                </div>
+            </Link>
+        </article>
+    );
+};
+
+export default PostPreview;
--- frontend/src/pages/projects.js
+++ frontend/src/pages/projects.js
@@ -1,23 +1,43 @@
 import React from "react";
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
+import styled from "styled-components";
+
 import Layout from "../components/layout";
+import ProjectPreview from "../components/ProjectPreview";
 
 const ProjectsPage = ({ data }) => {
     const projects = data.allStrapiProject.edges;
     return (
         <Layout>
-            {projects.map(({ node: { project_name, id, slug } }) => {
-                return (
-                    <Link to={`/project/${slug}`}>
-                        <div key={id}>
-                            <h2>{project_name}</h2>
-                        </div>
-                    </Link>
-                );
-            })}
+            <GridContainer>
+                {projects.map(
+                    ({ node: { project_name, id, slug, title_image } }) => {
+                        return (
+                            <ProjectPreview
+                                key={id}
+                                title={project_name}
+                                slug={slug}
+                                image={title_image.localFile}
+                                imageAlt={title_image.alternativeText}
+                            />
+                        );
+                    }
+                )}
+            </GridContainer>
         </Layout>
     );
 };
+
+const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+    gap: 1rem;
+    margin: 2rem;
+    @media screen and (max-width: 756px) {
+        margin: 1rem;
+        grid-template-columns: 1fr;
+    }
+`;
 
 export default ProjectsPage;
 
@@ -29,6 +49,14 @@ export const query = graphql`
                     project_name
                     id
                     slug
+                    title_image {
+                        alternativeText
+                        localFile {
+                            childImageSharp {
+                                gatsbyImageData(aspectRatio: 1.78)
+                            }
+                        }
+                    }
                 }
             }
         }
