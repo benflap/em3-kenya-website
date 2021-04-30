53149400b50d662a886e88f61e9b95706441bf70
ben <benflap@protonmail.com>
2021-04-30 19:33:34 -0400

    fix(deploy): configure for deployment

--- cms/config/server.js
+++ cms/config/server.js
@@ -1,9 +1,13 @@
+const dev = process.env.NODE_ENV === "development";
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '15e62ab23100b0f21ea9dc04b60dca96'),
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    url: dev ? "" : "https://em3international.org/kenya/api",
+    admin: {
+        url: dev ? "admin" : "https://em3international.org/kenya/admin",
+        auth: {
+            secret: env("ADMIN_JWT_SECRET", "15e62ab23100b0f21ea9dc04b60dca96"),
+        },
     },
-  },
 });
