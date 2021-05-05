b4e2693cc743b2b6ec1c390891a8f60ab7d0251c
ben <benflap@protonmail.com>
2021-05-05 16:10:48 -0400

    get frontend ready to deploy

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,9 +1,16 @@
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+});
+
+const { STRAPI_API_URL = "http://localhost:1337" } = process.env;
+
 module.exports = {
+    pathPrefix: `/kenya`,
     plugins: [
         {
             resolve: "gatsby-source-strapi",
             options: {
-                apiURL: "http://localhost:1337",
+                apiURL: STRAPI_API_URL,
                 singleTypes: ["home-page", "mission"],
                 contentTypes: [{ name: "blog", endpoint: "blog" }, "project"],
                 queryLimit: 1000,
@@ -15,7 +22,7 @@ module.exports = {
             options: {
                 name: `EM3 Kenya`,
                 short_name: `EM3 Kenya`,
-                start_url: `/`,
+                start_url: `/kenya`,
                 background_color: `#fff`,
                 theme_color: `#D3AC12`,
                 display: `browser`,
